a12877e8175dd66e906396c858a6d440b5932a09
Jesse Stricker <978183+jessestricker@users.noreply.github.com>
2022-07-04 02:50:45 +0200

    feat: add logging and command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+use clap_verbosity_flag::{Verbosity, WarnLevel};
+use log::info;
+
+#[derive(Debug, Parser)]
+#[clap(version, author, about, bin_name = env!("CARGO_BIN_NAME"))]
+struct Cli {
+    #[clap(flatten)]
+    verbosity: Verbosity<WarnLevel>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // parse CLI args
+    let cli = Cli::parse();
+
+    // set up logger
+    env_logger::Builder::new()
+        .filter_level(cli.verbosity.log_level_filter())
+        .format_target(false)
+        .format_timestamp(None)
+        .init();
+
+    info!("Hello, world!")
 }
